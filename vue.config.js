0baad512a4ec6ec3ce68a7a1a948727291cca005
PatrickMalara <pat_malara@outlook.com>
2020-10-09 22:24:50 -0400

    add github pages deploy files

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/mongoal-vue2'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/mongoal-vue2/'
     : '/'
 }
